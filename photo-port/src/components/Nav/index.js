1c12847499389e44a57106651f07de100257aeb9
Cortnie <cortniemuscari@gmail.com>
2022-06-15 22:33:03 -0500

    gallery

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -1,55 +1,58 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-    const {
-        categories = [],
-        setCurrentCategory,
-        currentCategory,
-    } = props;
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
-    // first argument is the callback function to be executed, and the second arguement directs the hook to re-render the component on any changes to the state of currentCategory
-    // ie - if currentCategory changes, the component will re-render so that the change in document.title will be visible to the user
-    useEffect(() => {
-        document.title = capitalizeFirstLetter(currentCategory.name);
-    }, [currentCategory]);
 
-    return (
-        <header className="flex-row px-1">
-            <h2>
-                <a data-testid="link" href="/">
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-                </a>
-            </h2>
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a data-testid="about" href="#about">
-                            About me
-                        </a>
-                    </li>
-                    <li className="mx-2">
-                        <span>Contact</span>
-                    </li>
-                    {categories.map((category) => (
-                        <li
-                            className={`mx-1 ${currentCategory.name === category.name && 'navActive'
-                                }`}
-                            key={category.name}
-                        >
-                            <span
-                                onClick={() => {
-                                    setCurrentCategory(category)
-                                }}
-                            >
-                                {capitalizeFirstLetter(category.name)}
-                            </span>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    );
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about">
+              About me
+            </a>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick('Contact')}>
+              Contact
+            </span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
